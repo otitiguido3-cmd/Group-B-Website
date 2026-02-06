538d61ed9117ea21ff6b312279d53970f03fb996
otitiguido3-cmd <otitiguido3@gmail.com>
2026-02-06 13:21:19 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -26,3 +26,41 @@ function calc() {
     //console.log(calculate);
     document.querySelector("#results").innerHTML = calculate
 }
+
+
+
+/*DYNAMIC MESSAGE INSIDE THE WEBSITE*/
+let getHeader = document.querySelector(".greeting");
+
+//Creating a new element
+let newElement = document.createElement("h1");
+
+//Creating the new date object and the the current hour
+let date = new Date();
+let currentHour = date.getHours();
+let createTxtMsg ; // Empty variable for messages
+
+//Conditional statements
+if (currentHour >= 4 && currentHour < 12) {
+    createTxtMsg = "Good Morning, Champion!!"
+}else if (currentHour >= 12 && currentHour < 18) {
+    createTxtMsg = "Goood Afternoon!!"
+}else if (currentHour >= 18 && currentHour < 21) {
+    createTxtMsg = "Goood Evening!!"
+}else if (currentHour >= 21 && currentHour < 4) {
+    createTxtMsg = "Goood Night !!"
+}else{ 
+    createTxtMsg = "Are you from another planet if not then check on your time!!"
+}
+console.log(createTxtMsg);
+
+
+//Creating the new text node
+let newTxtNode = document.createTextNode(createTxtMsg);
+
+//Appending the text to the new element
+newElement.appendChild(newTxtNode);
+
+//Displaying the content inside the website
+getHeader.appendChild(newElement);
+
